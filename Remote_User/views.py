2eea59caf194f1c912d937a8e896bca8c739f9f9
veeta03 <veetareddypodduturu@gmail.com>
2026-06-06 04:05:35 +0530

    Fix HttpResponse import

--- Remote_User/views.py
+++ Remote_User/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Count
 from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 import datetime
 import openpyxl
 import numpy as np
