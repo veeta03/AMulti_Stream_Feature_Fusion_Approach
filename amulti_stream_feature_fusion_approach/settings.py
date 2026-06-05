27944b39507f6ce5e822bc6dbec146d7379a35a5
veeta03 <veetareddypodduturu@gmail.com>
2026-06-06 02:05:50 +0530

    Fix whitenoise storage

--- amulti_stream_feature_fusion_approach/settings.py
+++ amulti_stream_feature_fusion_approach/settings.py
@@ -128,4 +128,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Template/media')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
